4b0d0d6bde94cc9fb2d08dae0b6cec82588e6840
threehams <threehams@gmail.com>
2017-04-14 22:37:12 -0700

    Change to cross-env.

--- src/__test__/configureExpect.ts
+++ src/__test__/configureExpect.ts
@@ -2,9 +2,7 @@ import 'mocha';
 import '../polyfills';
 
 import * as chai from 'chai';
-import * as chaiImmutable from 'chai-immutable';
 
-chai.use(chaiImmutable);
 chai.config.truncateThreshold = 0;
 
 export { expect } from 'chai';
